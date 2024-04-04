e269985be4a681fa994cd5761adf5d2f18a31835
neyirerdeser <neyirerdeser@gmail.com>
2024-04-04 16:52:06 -0400

    cleanup

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,63 +1,37 @@
-const uuid = require("uuid").v4; // versions are different kinds of ids, v4 has a timestamp component in it
 const { validationResult } = require("express-validator");
 const mongoose = require("mongoose");
 
-const HttpError = require("../models/http-error");
 const getCoordsForAdress = require("../util/location");
+
+const HttpError = require("../models/http-error");
 const Place = require("../models/place");
 const User = require("../models/user");
 
-/*
-let DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "ESB",
-    desc: "empire  states building",
-    address: "West 34th Street, New York, NY, USA",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584,
-    },
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "ESB",
-    desc: "empire  states building",
-    address: "West 34th Street, New York, NY, USA",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584,
-    },
-    creator: "u1",
-  },
-];
-*/
-
 const getPlaceById = async (req, res, next) => {
-  const placeId = req.params.pid; // { pid: p1 }
+  const placeId = req.params.pid;
+
   let place;
   try {
     place = await Place.findById(placeId);
   } catch (error) {
-    return next(new HttpError("something went wrong", 500));
+    return next(new HttpError(error, 500));
   }
   if (!place) return next(new HttpError("could not find said place", 404));
-  res.json({ place: place.toObject({ getters: true }) }); // { place } => { place: place } // getters make the id readable in this case
+
+  res.json({ place: place.toObject({ getters: true }) }); // getters make the id readable in this case
 };
 
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
+
   let user;
   try {
-    user = await User.findById(userId).populate('places');
+    user = await User.findById(userId).populate("places");
   } catch (error) {
-    return next(new HttpError("something went wrong", 500));
+    return next(new HttpError(error, 500));
   }
-
   if (!user || user.places.length === 0)
     return next(new HttpError("no places exist for such user", 404));
-  // next must be used if running async funcs. // returning so that the rest doesnt run
 
   res.json({
     places: user.places.map((place) => place.toObject({ getters: true })),
@@ -65,11 +39,11 @@ const getPlacesByUserId = async (req, res, next) => {
 };
 
 const createPlace = async (req, res, next) => {
-  // needs async bc were working with promises now, also using await and next (instead of throw)
   const errors = validationResult(req);
   if (!errors.isEmpty()) return next(new HttpError("invalid inputs", 422)); // unprocessible entry
 
   const { title, desc, address, creator } = req.body;
+
   let coordinates;
   try {
     coordinates = await getCoordsForAdress(address);
@@ -81,19 +55,18 @@ const createPlace = async (req, res, next) => {
   try {
     loggedUser = await User.findById(creator);
   } catch (error) {
-    return next(new HttpError("user check failed", 500));
+    return next(new HttpError(error, 500));
   }
   if (!loggedUser) return next(new HttpError("no such user", 404));
-  console.log(loggedUser);
+
   const createdPlace = new Place({
-    // id: uuid(),
     title,
     description: desc,
     address,
     location: coordinates,
     creator,
   });
-  console.log(createdPlace);
+
   try {
     // IMPORTANT: transaction requires that the collection is pre-existing (i.e places for Place item to be saved)
     const session = await mongoose.startSession();
@@ -103,8 +76,9 @@ const createPlace = async (req, res, next) => {
     await loggedUser.save({ session });
     await session.commitTransaction();
   } catch (error) {
-    return next(new HttpError("could not create place " && error, 500));
+    return next(new HttpError(error, 500));
   }
+
   res.status(201).json({ place: createdPlace });
 };
 
@@ -119,7 +93,7 @@ const updatePlaceById = async (req, res, next) => {
   try {
     place = await Place.findById(placeId);
   } catch (error) {
-    return next(new HttpError("something went wrong", 500));
+    return next(new HttpError(error, 500));
   }
   if (!place) return next(new HttpError("could not find said place", 404));
 
@@ -129,18 +103,20 @@ const updatePlaceById = async (req, res, next) => {
   try {
     await place.save();
   } catch (error) {
-    return next(new HttpError("couldnt update place", 500));
+    return next(new HttpError(error, 500));
   }
+
   res.status(200).json({ place: place.toObject({ getters: true }) });
 };
 
 const deletePlaceById = async (req, res, next) => {
   const placeId = req.params.pid;
+
   let placeToDelete;
   try {
     placeToDelete = await Place.findById(placeId).populate("creator");
   } catch (error) {
-    return next(new HttpError("something went wrong1", 500));
+    return next(new HttpError(error, 500));
   }
   if (!placeToDelete) return next(new HttpError("place does not exist", 404));
 
@@ -148,11 +124,11 @@ const deletePlaceById = async (req, res, next) => {
     const session = await mongoose.startSession();
     await session.startTransaction();
     placeToDelete.creator.places.pull(placeToDelete);
-    await placeToDelete.creator.save({session});
+    await placeToDelete.creator.save({ session });
     await placeToDelete.deleteOne({ session });
     await session.commitTransaction();
   } catch (error) {
-    return next(new HttpError("something went wrong", 500));
+    return next(new HttpError(error, 500));
   }
 
   res.status(200).json({ message: "place deleted" });
--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,16 +1,7 @@
-const HttpError = require("../models/http-error");
-const uuid = require("uuid").v4;
 const { validationResult } = require("express-validator");
-const User = require("../models/user");
 
-// const DUMMY_USERS = [
-//   {
-//     id: "u1",
-//     name: "neyir e",
-//     email: "a@b.c",
-//     password: "1234",
-//   },
-// ];
+const HttpError = require("../models/http-error");
+const User = require("../models/user");
 
 const getUsers = async (req, res, next) => {
   let users;
@@ -19,9 +10,7 @@ const getUsers = async (req, res, next) => {
   } catch (error) {
     return next(new HttpError("could not find users", 500));
   }
-
   if (users.length === 0) return next(new HttpError("no users exist", 404));
-  // next must be used if running async funcs. // returning so that the rest doesnt run
 
   res.json({ users: users.map((user) => user.toObject({ getters: true })) });
 };
@@ -31,13 +20,6 @@ const signup = async (req, res, next) => {
     return next(new HttpError("invalid inputs", 422));
 
   const { username, email, password } = req.body;
-  // let existingUser;
-  // try {
-  //   existingUser = await User.findOne({ email: email });
-  // } catch (error) {
-  //   return next(new HttpError("couldnt check existing user", 500));
-  // }
-  // if (existingUser) return next(new HttpError("user already exists!", 422)); // invalid user input
 
   const createdUser = new User({
     name: username,
@@ -49,16 +31,24 @@ const signup = async (req, res, next) => {
   try {
     await createdUser.save();
   } catch (error) {
-    return next(new HttpError("failed to create user", 500));
+    return next(new HttpError(error, 500));
   }
+
   res.status(201).json({ user: createdUser });
 };
 
 const login = async (req, res, next) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email: email });
+
+  let user;
+  try {
+    user = await User.findOne({ email: email });
+  } catch (error) {
+    return next(new HttpError(error, 500));
+  }
   if (!user || user.password !== password)
     return next(new HttpError("credentials not matching", 401)); // failed auth
+
   res.json({ message: "logged in!" });
 };
 
